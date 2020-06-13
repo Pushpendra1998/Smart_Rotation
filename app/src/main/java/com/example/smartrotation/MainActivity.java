6de66b966a3ff4a2dd5e22fd91dd01d04e977423
Payal Lodhi <payal.lodhi@vemo.com>
2020-06-14 01:03:12 +0530

    1. Remove Extra Files. 2. Add Foreground service. 3. Add Permission for drawable and app usage.

--- app/src/main/java/com/example/smartrotation/MainActivity.java
+++ app/src/main/java/com/example/smartrotation/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.smartrotation;
 
+import android.app.AlertDialog;
+import android.app.AppOpsManager;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
+import android.provider.Settings;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,9 +27,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int OVERLAY_PERMISSION_REQ_CODE = 1234;
+    public static int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 12345;
+    Switch enableRotationSwitch;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
@@ -31,21 +44,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Check permissions first
+        checkPermissions();
+        enableRotationSwitch = findViewById(R.id.enable_smart_rotation_switch);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -65,5 +72,93 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void checkPermissions() {
+        // Function to check permission
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                OverlayPermissionDialogFragment dialogFragment = new OverlayPermissionDialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "Overlay Permission");
+            }
+            else if(!hasUsageStatsPermission()){
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                UsageAcessDialogFragment dialogFragment = new UsageAcessDialogFragment();
+                ft.add(dialogFragment, null);
+                ft.commitAllowingStateLoss();
+            }
+        }
+    }
+
+    private boolean hasUsageStatsPermission() {
+        AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+        int mode = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                    android.os.Process.myUid(), getPackageName());
+        }
+        return mode == AppOpsManager.MODE_ALLOWED;
+    }
+    public static class OverlayPermissionDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.ovarlay_permission_description)
+                    .setTitle("Overlay Permission")
+                    .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                    Uri.parse("package:" + getActivity().getPackageName()));
+                            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+                        }
+                    });
+
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
+    public static class UsageAcessDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            builder.setMessage(R.string.usage_data_access_description)
+                    .setTitle("Usage Access Permission")
+                    .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // FIRE ZE MISSILES!
+                            startActivityForResult(
+                                    new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),
+                                    MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS);
+                        }
+                    });
+
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+
+    public void enableSmartRotationService(View view){
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+        if (((Switch) view).isChecked()){
+            Intent serviceIntent = new Intent(this, SmartRotationService.class);
+            startService(serviceIntent);
+            myEdit.putString("Gesture Status", "true");
+            myEdit.commit();
+            Toast.makeText(this, "Enabled", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent serviceIntent = new Intent(this, SmartRotationService.class);
+            stopService(serviceIntent);
+            myEdit.putString("Gesture Status", "false");
+            myEdit.commit();
+            Toast.makeText(this, "Disabled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
